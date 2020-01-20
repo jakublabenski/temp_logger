b463269022f6cba6700d7e105862fc7176ee9007
jakub <25360840+jakublabenski@users.noreply.github.com>
2020-01-20 17:47:20 +0100

    feat: replace WifiManager with IotWebConfig

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,19 @@
 
 #include <DNSServer.h> //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h> //Local WebServer used to serve the configuration portal
-#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
 #include <PubSubClient.h>
 
+#include <IotWebConf.h>
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+DNSServer dnsServer;
+WebServer server(80);
+
+IotWebConf iotWebConf("temp_sensor", &dnsServer, &server, "password");
+
 char msg[50];
 
 const char* topic = "/temp_logger";
@@ -20,30 +26,36 @@ void setup()
 {
     Serial.begin(9600);
 
-    WiFiManager wifiManager;
-    wifiManager.autoConnect("temp_logger");
+    iotWebConf.init();
+
+    // -- Set up required URL handlers on the web server.
+//    server.on("/", handleRoot);
+    server.on("/", []{ iotWebConf.handleConfig(); });
+    server.onNotFound([](){ iotWebConf.handleNotFound(); });
 
     client.setServer("192.168.0.3", 1883);
+    auto a = WiFi.localIP();
+    a.toString().toCharArray(msg, 50);
+    Serial.printf("IP: [%s]\n", msg);
+    Serial.printf("IP: [%s]\n", msg);
+    WiFi.SSID().toCharArray(msg, 50);
+    Serial.printf("SSID: [%s]\n", msg);
+
 }
 
 void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
+    // Loop until we're reconnected
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
+        Serial.println("connected");
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
     }
-  }
 }
 
 bool one_wire_loop(int64_t& ret_addr, float& celsius);
@@ -68,12 +80,15 @@ void reading()
 
 void loop()
 {
-    if (!client.connected()) {
-        reconnect();
-    }
-    client.loop();
+    iotWebConf.doLoop();
 
-    reading();
+    if (WiFi.isConnected()) {
+        if (!client.connected()) {
+            reconnect();
+        }
+        client.loop();
+        reading();
+    }
 
     Serial.println("Done");
     delay(10000);
